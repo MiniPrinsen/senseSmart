afc3960c9418d15a56470dbbf0964ed535a3f5ca
Viktor Ahlund <id14vad@cs.umu.se>
2017-06-16 15:27:48 +0200

    Hej du

--- src/java/MessageServlet.java
+++ src/java/MessageServlet.java
@@ -38,6 +38,12 @@ public class MessageServlet extends HttpServlet {
         Collection<Part> parts = request.getParts();
         CityObject cityobject = new CityObject();
         
+        if(request.getParameter("longitude") != null && request.getParameter("latitude") != null) {
+            System.out.println("HEJ_VIKTOR");
+        } 
+        
+        else {
+        
        /* String imageURL = "/Users/gustafwennerstrom/Documents/Jobb/SenseSmart/Databas"
                             + "/images/";*/
        out.println(getServletContext().getRealPath("/"));
@@ -87,7 +93,8 @@ public class MessageServlet extends HttpServlet {
         out.print(cityobject.toString());
         addCityObjectToDatabase(cityobject);
      
-    }
+        }
+        }
         
     /**
      * 
