e971aaad066abc10ec60cfb093ea1b5596b8ad9d
Gustaf Wennerström <gustafwennerstrom@Gustafs-MBP.lan>
2017-06-22 10:41:01 +0200

    Fix och Trix. Mappade urler till bilder och inläsning av java-objekt med JSON. Och POST svarar med JSON

--- src/java/CityObject.java
+++ src/java/CityObject.java
@@ -11,69 +11,121 @@ import java.util.ArrayList;
  *
  * @author gustafwennerstrom
  */
+ 
 public class CityObject {
-    private String name;
-    private String description;
-    private ArrayList<String> images;
-    private String longitude;
-    private String latitude;
-    
-    public int getLength(){
-        return images.size();
-    }
-    
-    public CityObject(){
-        this.images = new ArrayList<>();
-    }
-    
-    public void addImagePath(String image){
-        images.add(image);
-    }
-    
-    public ArrayList<String> getImagePaths(){
-        return images;
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    public String getName(){
-        return this.name;
-    }
-    
-    public void setDescription(String description){
-        this.description = description;
-    }
-    
-    public String getDescription(){
-        return this.description;
-    }
-    
-    public void setLongitute(String longitude){
-        this.longitude = longitude;
-    }
-    
-    public String getLongitude(){
-        return longitude;
-    }
-    
-    public void setLatitude(String latitude){
-        this.latitude = latitude;
-    }
-    
-    public String getlatitude(){
-        return latitude;
-    }
-    
-    @Override
-    public String toString(){
-        String info = "Name: "+this.name+"\nDescription: "+this.description+
-                "\nLongitude: "+this.longitude+"\nLatitude: "+this.latitude;
-        for (String path : this.images){
-            info = info + "\n"+path;
+	String name;
+        private String description;
+	private Oid _id;
+        private Coordinates coordinates;
+        private double rating;
+        private int persons_voted;
+        private ArrayList<String> images;
+        
+        public CityObject(){
+            images = new ArrayList<>();
+            coordinates = new Coordinates();
         }
         
-        return info;
-    }
+        public void addImagePath(String imgPath){
+            images.add(imgPath);
+        }
+        
+        public void setDescription(String desc){
+            description = desc;
+        }
+        
+        public ArrayList<String> getImages(){
+            return images;
+        }
+        
+        public String getDescription(){
+            return description;
+        }
+        
+        public double getRating(){
+            return rating;
+        }
+        
+        public int getPersons_voted(){
+            return persons_voted;
+        }
+        
+        
+        public Oid getOid(){
+            return _id;
+        }
+        
+        public Coordinates getCoordinates(){
+            return coordinates;
+        }
+        
+        public void setLongitude(double lng){
+            coordinates.setLongitude(lng);
+        }
+        
+         public void setLatitude(double lat){
+            coordinates.setLatitude(lat);
+        }
+        
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Mongodb will automatically generate ObjectId
+	 * @author fhp
+	 *
+	 */
+	public class Oid{
+		String $oid;
+		public String get$oid() {
+			return $oid;
+		}
+ 
+		public void set$oid(String $oid) {
+			this.$oid = $oid;
+		}
+ 
+	}
+        
+        public class Coordinates{
+            double longitude;
+            double latitude;
+            
+            public double getLongitude(){
+                return this.longitude;
+            }
+            
+            public void setLongitude(double longitude){
+                this.longitude = longitude;
+            }
+            
+            public double getLatitude(){
+                return this.latitude;
+            }
+            
+            public void setLatitude(double latitude){
+                this.latitude = latitude;
+            }
+        }
+        
+        public String toHTML(){
+            StringBuilder htmlContent = new StringBuilder();
+            htmlContent.append("<div id ='cityObject'>");
+            htmlContent.append("<h1>"+name+"</h1>");
+            for (String imgUrl : images){
+                htmlContent.append("<img src='"+imgUrl+"'</img>");
+            }
+            htmlContent.append("<p>"+description+"</p>");
+            htmlContent.append("<p><b>Longitude: </b>"+coordinates.getLongitude()+"</p>");
+            htmlContent.append("<p><b>Latitude: </b>"+coordinates.getLatitude()+"</p>");
+            htmlContent.append("<p><b>Antal röster: </b>"+persons_voted+"</p>");
+            htmlContent.append("<p><b>Rating: </b>"+rating+"</p>");
+            htmlContent.append("</div>");
+            return htmlContent.toString();
+
+        }
 }
--- src/java/MessageServlet.java
+++ src/java/MessageServlet.java
@@ -1,7 +1,15 @@
 /*TJENA?????*/
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+/*import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;*/
+
 import java.io.*;
 import javax.servlet.*;
+import java.util.UUID;
 import javax.servlet.http.*;
 
 import com.mongodb.MongoClient;
@@ -9,17 +17,28 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
 import java.awt.image.BufferedImage;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 import javax.imageio.ImageIO;
 import javax.servlet.annotation.MultipartConfig;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 @MultipartConfig
 public class MessageServlet extends HttpServlet {
     private PrintWriter out;
+    /*
+    * visibleRadius: Cityobjects will be visible within this radius (meter).  
+    */
+    private final int visibleRadius = 5000;
 
         
     /**
@@ -35,62 +54,99 @@ public class MessageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         out = response.getWriter(); 
-        Collection<Part> parts = request.getParts();
-        CityObject cityobject = new CityObject();
-        //addCityObjectToDatabase(cityobject);
-
         
-       /* String imageURL = "/Users/gustafwennerstrom/Documents/Jobb/SenseSmart/Databas"
-                            + "/images/";*/
-       //out.println(getServletContext().getRealPath("/"));
-
-        String imageURL = "../../Databas/images/";
-        int images = 1;
-        out.println(parts.size());
-        if ( parts.size() >= 5){
-            for (Part p : parts){
-                String type = p.getName();
-                switch (type){
-                    case "name":
-                        cityobject.setName(getStringFromInputStream(
-                        p.getInputStream()));
-                        out.println("NAME is: "+cityobject.getName());
-                        break;
-                    case "description":
-                        cityobject.setDescription(
-                                getStringFromInputStream(p.getInputStream()));
-                        break;
-                    case "longitude":
-                        cityobject.setLongitute(getStringFromInputStream(
-                                p.getInputStream()).replace(",", "."));
-                        break;
-                    case "latitude":
-                        cityobject.setLatitude(getStringFromInputStream(
-                                p.getInputStream()).replace(",", "."));
-                        break;
-                    case "image":
-                        BufferedImage bufferedImage = ImageIO.read(p.getInputStream());
-                        String filePath = imageURL + cityobject.getName() + "-"
-                                +images+".png";
-                        cityobject.addImagePath(filePath);
-                        ImageIO.write(bufferedImage, "png", new File(filePath));
-                        images++;
-                        break;  
-
-                }
-
-            }
+        String longitude = request.getParameter("longitude");
+        String latitude = request.getParameter("latitude");
+        
+        if(longitude != null && latitude != null){
+            double lng = Double.parseDouble(longitude);
+            double lat = Double.parseDouble(latitude);
+            
+            /* Response as JSON. */
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            out.write(getCityObjectsJson(lat, lng));
+        
         }
         
         else{
-            out.println("Du måste fylla i alla fält för att ladda in"
-                    + "ett nytt objekt.");
+            
+            Collection<Part> parts = request.getParts();
+            CityObject cityobject = new CityObject();
+
+            String imageURL = "../../Databas/images/";
+            out.println(parts.size());
+            if ( parts.size() >= 5){
+                for (Part p : parts){
+                    String type = p.getName();
+                    switch (type){
+                        case "name":
+                            cityobject.setName(getStringFromInputStream(
+                            p.getInputStream()));
+                            out.println("NAME is: "+cityobject.getName());
+                            break;
+                        case "description":
+                            cityobject.setDescription(
+                                    getStringFromInputStream(p.getInputStream()));
+                            out.println("DESC IS: "+cityobject.getDescription());
+                            break;
+                        case "obj_longitude":
+                            out.println("IN LNG");
+                            double lng = Double.parseDouble(getStringFromInputStream(
+                            p.getInputStream()).replace(",", "."));
+                            out.println(lng);
+                            cityobject.setLongitude(lng);
+                            out.println("AFTR LNG");
+
+                            break;
+                        case "obj_latitude":
+                            out.println("IN LAT");
+                            double lat = Double.parseDouble(getStringFromInputStream
+                                (p.getInputStream()).replace(",", "."));
+                            cityobject.setLatitude(lat);
+                            out.println("LAT IS: "+cityobject.getCoordinates().getLatitude());
+
+                            break;
+                        case "image":
+                            String imgName = UUID.randomUUID().toString();
+                            BufferedImage bufferedImage = ImageIO.read(p.getInputStream());
+                            String filePath = imageURL + imgName+".png";
+                            
+                            /* Create a unique filename (not human readable) */
+                            Path image = Paths.get(filePath);
+                            while(Files.exists(image)){
+                                imgName = UUID.randomUUID().toString();
+                                filePath = imageURL + imgName + ".png";
+                                image = Paths.get(filePath);
+                            }
+                            
+                            /* Http-URL for image (mapped in context.xml) */
+                            URI imgURL = URI.create(request.getRequestURL().
+                                    toString()).resolve(request.getContextPath()
+                                            +"/images/"+imgName+".png");
+                                   
+                            
+                            
+                            /* Save image to object and file-system. */
+                            cityobject.addImagePath(imgURL.toString());
+                            ImageIO.write(bufferedImage, "png", new File(filePath));
+                            out.print("IMAGE SAVED");
+                            break;  
+
+                    }
+
+                }
+            }
+
+            else{
+                out.println("Du måste fylla i alla fält för att ladda in"
+                        + "ett nytt objekt.");
+            }
+            out.print(cityobject.toString());
+            addCityObjectToDatabase(cityobject);
         }
-        out.print(cityobject.toString());
-        addCityObjectToDatabase(cityobject);
-     
         
-        }
+    }
         
     /**
      * 
@@ -108,29 +164,20 @@ public class MessageServlet extends HttpServlet {
     throws ServletException, IOException {
         
         out = response.getWriter();
-        
-        addCityObjectToDatabase(new CityObject());
-        
-        String longitude = request.getParameter("longitude");
-        String latitude = request.getParameter("latitude");
+        response.setContentType("text/html");
 
-        if (longitude != null){
-            out.println("Longitude "+longitude+"?");
+        String uri = request.getRequestURI();
+        if (uri.equals("/senseSmart/cityobjects")){
+            getCityObjects();
         }
-        if (latitude != null){
-            out.println("Latitude "+latitude+"?");
-        }
-        out.println("Ska soka efter grejer i frankfurt");
-        
-      //  doStuff();
-
-        out.println("Success!");
-
     }
+  
     
     private void addCityObjectToDatabase(CityObject cityObject){
         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
 
+    //    Gson gson = new Gson();
+//String json = gson.toJson(Employee);    
         MongoDatabase database = mongoClient.getDatabase("test");
 
         MongoCollection cityobjectsCol = database.getCollection("cityobjects");
@@ -140,20 +187,17 @@ public class MessageServlet extends HttpServlet {
                "description", cityObject.getDescription()
                ).append(
                "coordinates", new Document().append(
-                       "longitude", cityObject.getLongitude()).append(
-                       "latitude", cityObject.getlatitude())
-                ).append("persons_voted", 0).append("rating", 0).append("images", cityObject.getImagePaths()));
+                       "longitude", cityObject.getCoordinates().getLongitude()).append(
+                       "latitude", cityObject.getCoordinates().getLatitude())
+                ).append("persons_voted", 0).append("rating", 0).append("images", cityObject.getImages()));
              
-     /* cityobjectsCol.insertOne(new Document("address",
-                new Document()
-                        .append("salutation", "HEJDÅÅ"
-                )));*/
+
     }
     
         /**
          *  Checks if a location is in a specific radius
          */
-       public static boolean inRadius(double lat1, double lng1, double lat2, double lng2, int radius) {
+       public boolean inRadius(double lat1, double lng1, double lat2, double lng2, int radius) {
         double earthRadius = 6371000; //meters
         double dLat = Math.toRadians(lat2-lat1);
         double dLng = Math.toRadians(lng2-lng1);
@@ -172,61 +216,7 @@ public class MessageServlet extends HttpServlet {
             return false;
         }
     }
-       
-       
-       public void doStuff(){
-        
-           try{   	
-         // To connect to mongodb server
-         MongoClient mongoClient = new MongoClient( "172.31.20.157" , 27017 );
-			
-         // Now connect to your databases
-        
-         out.println("Connect to database successfully");
-         
-         MongoDatabase database = mongoClient.getDatabase("test");
-         
-            MongoCollection<Document> collection = database.getCollection("cityobjects");
-            out.println(collection.count());
-            out.println("HEJHEJ den är igenom!!");
-          
-      }
-        catch(Exception e){
-         out.println(e.getClass().getName() + ": " + e.getMessage());
-      }
-        
-   }
-       
-       public void testGetDataFromDatabase(){
-        String storedText = "";
-        
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
 
-        MongoDatabase database = mongoClient.getDatabase("test");
-
-        MongoCollection collec1 = database.getCollection("cityobjects"); 
-        
-        
-        FindIterable<Document> iter = collec1.find();
-        
-        MongoCursor it;
-        
-        it = iter.iterator();
-        
-        Document as;
-        
-        
-        while (it.hasNext()){
-            
-            as = (Document)it.next();
-            storedText = as.toString();
-            as.remove("_id");
-            out.println(as.toJson()+"HEJ");
-        }
-        
-       }
-       
-       
        /**
         * 
         * Creates a string from inputstream
@@ -254,8 +244,112 @@ public class MessageServlet extends HttpServlet {
             return sb.toString();
        
        }
+       
+       /**
+        * Get city objects from the database formatted as a HTML-page. 
+        */
+       private void getCityObjects(){
+        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> collection = database.getCollection("cityobjects");
+   
+        FindIterable<Document> iter = collection.find(eq("coordinates", eq("$exists",true)));
+            
+        
+        MongoCursor<Document> cursor = iter.iterator();
+        
+        ArrayList<CityObject> cityObjects = new ArrayList<>();
+        Gson gson = new Gson();
+        
+        while (cursor.hasNext()) {
+            Document temp = cursor.next();
+            CityObject testUser = gson.fromJson(temp.toJson(), CityObject.class);
+            cityObjects.add(testUser);
+        }
+        
+        cursor.close();
+        mongoClient.close();
+
+        /* Prints as HTML */
+         out.println("<!DOCTYPE html>");  // HTML 5
+         out.println("<html><head>");
+         out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n" +
+"    <link href=\"https://fonts.googleapis.com/css?family=Open+Sans\" rel=\"stylesheet\"/>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'></head>");
+         for (CityObject obj : cityObjects){
+            out.print(obj.toHTML());
+        }
+        out.print("</body></html>");
+        
+        
+       }
+       
+       /**
+        * 
+        * Gets cityobjects stored in a database (mongodb) based on
+        * a latitude and longitude.
+        * 
+        * @param latitude
+        * @param longitude
+        * @return JSON-formatted CityObject(s) close to the lat and lng.
+        */
+        private String getCityObjectsJson(double latitude, double longitude){
+            MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+             // Now connect to your databases
+
+            MongoDatabase database = mongoClient.getDatabase("test");
+
+            MongoCollection<Document> collection = database.getCollection("cityobjects");
+
+            FindIterable<Document> iter = collection.find(eq("coordinates", eq("$exists",true)));
 
 
+            MongoCursor<Document> cursor = iter.iterator();
 
+            ArrayList<CityObject> cityObjects = new ArrayList<>();
+            ArrayList<Document> tmpCities = new ArrayList<>();
+            while (cursor.hasNext()) {
+                Document temp = cursor.next();
+                Document coordinates = (Document)temp.get("coordinates");
+                if (coordinates != null){
+                    double cityObjectlatitude = (double)coordinates.get("latitude");
+                    double cityObjectlongitude = (double)coordinates.get("longitude");
+                    boolean inRadius = inRadius(cityObjectlatitude, 
+                            cityObjectlongitude, latitude, longitude, visibleRadius);
+                  //  if(inRadius){
+                    //    return temp.toJson();//out.write(temp.toJson());
+                    tmpCities.add(temp);
+                   // }
+
+                }
+            }
+            
+            cursor.close();
+            mongoClient.close();
+
+
+            StringBuilder jsonData = new StringBuilder();
+
+            if (!tmpCities.isEmpty()){
+                jsonData.append("[");
+                for (int i = 0; i<tmpCities.size(); i++){
+                    if (i != (tmpCities.size()-1)){
+                        jsonData.append(tmpCities.get(i).toJson()+",");
+                    }
+                    else{
+                        jsonData.append(tmpCities.get(i).toJson());
+                    }
+                }
+                jsonData.append("]");
+            }
+
+            return jsonData.toString();
+     }        
+            
 }
+
+
+
+
     
